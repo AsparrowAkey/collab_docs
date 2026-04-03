6dcd137405d3e46624cfd2de80b3c2a7893cb92b
WeightHear <businessroger1@proton.me>
2026-04-03 15:45:40 -0600

    Updated documents_live.ex phx-click feature

--- lib/collab_docs_web/live/documents_live.ex
+++ lib/collab_docs_web/live/documents_live.ex
@@ -10,10 +10,24 @@ defmodule CollabDocsWeb.DocumentsLive do
   end
 
   @impl true
+  def handle_event("create_document", _params, socket) do
+    {:ok, document} = Documents.create_document(%{
+      title: "Untitled Document",
+      content: ""
+    })
+
+    {:noreply, push_navigate(socket, to: "/documents/#{document.id}")}
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
     <div>
       <h1>Documents</h1>
+
+      <button phx-click="create_document">
+        + New Document
+      </button>
 
       <ul>
         <%= for document <- @documents do %>
